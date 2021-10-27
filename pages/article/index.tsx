1cee8dd26016c3e096eb5726465506b69450fb21
Ketan Iralepatil <50294674+KetanIP@users.noreply.github.com>
2021-10-27 22:25:23 +0530

    Update index.tsx

--- pages/article/index.tsx
+++ pages/article/index.tsx
@@ -29,7 +29,7 @@ const ArticlePage: NextPage = () => {
                 
                 {
                     article === "" &&
-                        <div>Loading<div>
+                        <div>Loading</div>
                 }
             
             </div>
