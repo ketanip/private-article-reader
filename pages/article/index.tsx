857396822c2ccfc1a9a53eac701a2341fea7b3a9
Ketan Iralepatil <Ketan Iralepatil>
2021-10-27 22:35:54 +0530

    Added loading spinner

--- pages/article/index.tsx
+++ pages/article/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import Loader from "react-loader-spinner";
 
 const ArticlePage: NextPage = () => {
 
@@ -12,26 +13,33 @@ const ArticlePage: NextPage = () => {
     axios.post("/api/get-article", {
         url: String(router.query.url)
     })
-    .then(resp => {
-        setArticle(resp.data.article);
-    })
-    .catch(err => console.log(err));
+        .then(resp => {
+            setArticle(resp.data.article);
+        })
+        .catch(err => console.log(err));
 
     return (
         <div className="container">
             <div className=" container article-container" >
-                
+
                 {
                     article !== "" &&
-                        <div dangerouslySetInnerHTML={{ __html: article }}></div>
+                    <div dangerouslySetInnerHTML={{ __html: article }}></div>
                 }
-                
-                
+
+
                 {
                     article === "" &&
-                        <div>Loading</div>
+                    <div className="loading">
+                        <Loader
+                            type="TailSpin"
+                            color="#000"
+                            height={100}
+                            width={100}
+                        />
+                    </div>
                 }
-            
+
             </div>
         </div>
     );
